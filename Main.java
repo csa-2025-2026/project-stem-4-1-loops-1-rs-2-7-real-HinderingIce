10c1cd06e1862efed0a3983eacdbedd76cd42385
HinderingIce <145399087+HinderingIce@users.noreply.github.com>
2025-10-17 18:17:40 +0000

    Commit

--- Main.java
+++ Main.java
@@ -4,6 +4,10 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+      int i = 3;
+      while(i <= 25)
+      {
+        System.out.println(i); i++; i++;
+      }
   }
 }
