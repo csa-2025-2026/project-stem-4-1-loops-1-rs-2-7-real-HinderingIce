e0e790066d64d2b57f1c35af71753ec99b6bd824
HinderingIce <145399087+HinderingIce@users.noreply.github.com>
2025-10-20 18:21:51 +0000

    COMMIT #2

--- Main.java
+++ Main.java
@@ -4,10 +4,58 @@ public class Main
 {
   public static void main(String[] args)
   {
-      int i = 3;
-      while(i <= 25)
+      // Scanner sc = new Scanner(System.in);
+      // System.out.println("Enter:");
+      // int userInput = sc.nextInt();
+      // int i = 1;
+
+      //Problem 1
+      // int sum = 0;
+      // while(i < 5)
+      // {
+      //     System.out.println("Enter:");
+      //     sum += userInput = sc.nextInt();
+      // }
+      //     System.out.println(sum);
+
+      // Problem 2
+      // int highest = 0;
+      // int i = 1;
+
+      // while(i < 5)
+      // {
+      //     System.out.println("Enter:");
+      //     userInput = sc.nextInt();
+      //     if(highest < userInput)
+      //     {
+      //       highest = userInput;
+      //     }
+      //     i++;
+      // }
+      // System.out.println();
+      // System.out.println(highest);
+      
+      // Problem 3
+      int index = 0;
+      String word = "calculator";
+      String e = ""; 
+      String currentLetter = "";
+      while(index < word.length())
       {
-        System.out.println(i); i++; i++;
+        if(index + 2 > word.length())
+        {
+          currentLetter = word.substring(index);
+        }
+        else
+        {
+          currentLetter = word.substring(index, index + 2);
+        }
+        index += 3;
+        e = e + currentLetter;
       }
+      System.out.println(e);
+      
+      // sc.close();
   }
 }
+
